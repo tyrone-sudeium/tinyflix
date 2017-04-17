c03c0eac8d524d5696241e58886cb77363036d5d
berwyn <berwyn.codeweaver@gmail.com>
2017-04-17 17:27:38 -0600

    feat: Adds CompactOverlay support

---
+++ Tinyflix/Common/BooleanToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Tinyflix.Common
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) =>
+            (bool)value ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+            (Visibility)value == Visibility.Visible ? true : false;
+
+    }
+
+}
