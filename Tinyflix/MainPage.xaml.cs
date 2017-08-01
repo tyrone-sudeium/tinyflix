aba300740eb17c0ff63e28d197649383aa85976f
Tyrone Trevorrow <tyrone@sudeium.com>
2017-08-01 23:32:18 +1000

    feat: hide compact overlay button for now

--- Tinyflix/MainPage.xaml.cs
+++ Tinyflix/MainPage.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI;
 using Windows.ApplicationModel.Core;
+using System.Threading;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,8 +27,12 @@ namespace Tinyflix
     public sealed partial class MainPage : Page
     {
         public bool CompactAvailable { get; private set; }
+        private bool ForceCompactHidden { get; set; }
 
         private bool _isCompact = false;
+        private bool _supportsCompact = false;
+        private bool _forceCompactHidden = false;
+        private Timer timer = null;
 
         public MainPage()
         {
@@ -40,7 +45,29 @@ namespace Tinyflix
             CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
             coreTitleBar.ExtendViewIntoTitleBar = true;
 
-            CompactAvailable = ApplicationView.GetForCurrentView().IsViewModeSupported(ApplicationViewMode.CompactOverlay);
+            _supportsCompact = ApplicationView.GetForCurrentView().IsViewModeSupported(ApplicationViewMode.CompactOverlay);
+            updateVisbilityBool();
+        }
+
+        private void updateVisbilityBool()
+        {
+            if (!_supportsCompact)
+            {
+                this.CompactAvailable = false;
+            }
+            else if (_isCompact)
+            {
+                this.CompactAvailable = false;
+            }
+            else if (_forceCompactHidden)
+            {
+                this.CompactAvailable = false;
+            }
+            else
+            {
+                this.CompactAvailable = true;
+            }
+            System.Diagnostics.Debug.WriteLine("CompactAvailable = " + this.CompactAvailable);
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
@@ -60,6 +87,40 @@ namespace Tinyflix
             {
                 _isCompact = !_isCompact;
             }
+            
         }
+
+        private void Page_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            _forceCompactHidden = true;
+            updateVisbilityBool();
+            startControlHideTimer();
+        }
+
+        private void Page_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            _forceCompactHidden = true;
+            updateVisbilityBool();
+            startControlHideTimer();
+        }
+
+        private void Page_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            startControlHideTimer();
+        }
+
+        private void startControlHideTimer()
+        {
+            if (this.timer != null)
+            {
+                this.timer.Dispose();
+            }
+            this.timer = new Timer((obj) =>
+            {
+                _forceCompactHidden = false;
+                updateVisbilityBool();
+            }, null, 3000, Timeout.Infinite);
+        }
+
     }
 }
